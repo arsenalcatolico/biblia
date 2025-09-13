13451708939d1928bbdf21a84b380641263988b3
Arsenal Católico <catolico.arsenal@gmail.com>
2025-09-13 16:02:35 +0000

    Try fixing this error: `Console Error: params are being enumerated. `par

--- src/app/(app)/leitura/[day]/page.tsx
+++ src/app/(app)/leitura/[day]/page.tsx
@@ -1,7 +1,7 @@
 
 "use client";
 
-import { useEffect, useState, use } from 'react';
+import { useEffect, useState } from 'react';
 import { useParams, useRouter } from 'next/navigation';
 import { ReadingDay } from '@/types';
 import { useSettings } from '@/contexts/SettingsContext';
@@ -298,4 +298,5 @@ export default function ReadingPage() {
   );
 }
 
+    
     

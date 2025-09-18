223946f882ffa3f1847fef7651bdee27eca399b6
Arsenal Católico <catolico.arsenal@gmail.com>
2025-09-18 18:39:16 +0000

    Console Error
    Error: Unexpected token '<', "<!DOCTYPE "... is not vali

--- src/lib/firebase/admin-config.ts
+++ src/lib/firebase/admin-config.ts
@@ -7,7 +7,7 @@ if (!admin.apps.length) {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
         // The private key must be formatted as a single-line string
-        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
       }),
     });
   } catch (error) {
